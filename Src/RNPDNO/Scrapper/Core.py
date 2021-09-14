9cb44e57bb943e38717172bc4b3fdf450dd25844
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2021-09-14 18:41:18 -0500

    Check if app configuration is loaded

--- Src/RNPDNO/Scrapper/Core.py
+++ Src/RNPDNO/Scrapper/Core.py
@@ -17,4 +17,24 @@ class Scrapper:
         
         self.__config_loaded = False
 
+    def check_config_loaded(self, error: bool = True) -> bool:
+        """Check if app configuration has been loaded
+
+        Args:
+            error (bool, optional): Should an exception be raised if the configuration is not loaded? Defaults to True.
+
+        Raises:
+            ConfigNotLoaded: If configuration is not loaded and error is set to True.
+
+        Returns:
+            bool: Configuration is loaded?
+        """
+
+        if self.__config_loaded:
+            return True
+        elif error:
+            raise self.Exceptions.ConfigNotLoaded("Configuration must be loaded first via the load_config method!")
+        else:
+            return False
+
         pass
