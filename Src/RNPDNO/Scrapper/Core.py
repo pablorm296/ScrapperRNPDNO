44ea4b8082ca61e8696bdec805fbfcc6aa70465c
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2021-09-14 18:40:33 -0500

    Add Scrapper exceptions

--- Src/RNPDNO/Scrapper/Core.py
+++ Src/RNPDNO/Scrapper/Core.py
@@ -1,5 +1,17 @@
 import requests
 
 class Scrapper:
+
+    class Exceptions:
+
+        class ConfigNotLoaded(Exception):
+            pass
+
+        class TemplateNotFound(Exception):
+            pass
+
+        class MultipleTemplatesFound(Exception):
+            pass
+
     def __init__(self) -> None:
         pass
